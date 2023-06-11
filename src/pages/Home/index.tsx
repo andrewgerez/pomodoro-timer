009310bd33402cb7d1080811eefa5f4690084cd2
Andrew Gerez <andrewgerez18@gmail.com>
2023-06-10 23:36:19 -0300

    pomodoro timer | function timer 100%

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -39,12 +39,18 @@ export const Home = () => {
   const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
 
   useEffect(() => {
+    let interval: number
+
     if (activeCycle) {
-      setInterval(() => {
+      interval = setInterval(() => {
         setAmountSecondsPassed(
           differenceInSeconds(new Date(), activeCycle.startDate),
         )
       }, 1000)
+    }
+
+    return () => {
+      clearInterval(interval)
     }
   }, [activeCycle])
 
@@ -60,6 +66,8 @@ export const Home = () => {
 
     setCycles((state) => [...state, newCycle])
     setActiveCycleId(id)
+    setAmountSecondsPassed(0)
+
     reset()
   }
 
